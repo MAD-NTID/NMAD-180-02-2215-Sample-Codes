b3873987c52c539547b7b3f39746cf05387badd8
kemoycampbell <ksc2650@rit.edu>
2022-01-14 12:04:53 -0500

    update hello world and fundamental group exercise with the coding standard as defined in the handout

---
+++ NMAD-180-02-Demo-Spring-2022/FundementalGroupExercise/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace FundementalGroupExercise
+{
+    /**
+     * Author: Kemoy Campbell       NMAD180-02
+     * Date: January 14, 2022
+     * Description: This program shows my name, age and major on the console.
+     *              It is a good introduction to the fundamental of c# and variables
+     * Caveats: None
+     */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //a variable to store the age
+            int age = 101;
+
+            //a variable to store the name
+            string name = "Kemoy Campbell";
+
+            //a variable to store the major 
+            string major = "Computer Science";
+
+            //remember to put the variables in the same order as the place holders {0}, {1} .. etc
+            //Also remember that in programming, counting start at 0 hence why the first place holder is {0}
+            //not {1}
+            Console.WriteLine("Hello my name is {0} and I am {1} years old. My major is {2}.", name, age, major);
+        }
+    }
+}
--- NMAD-180-02-Demo-Spring-2022/HelloWorld/Program.cs
+++ NMAD-180-02-Demo-Spring-2022/HelloWorld/Program.cs
@@ -1,16 +1,14 @@
 ﻿using System;
-
-/**
- * This is an hello world example.
- * In Programming, Hello World is often the first
- * program students write when learning programming
- * 
- * 
- * Feel free to play around the example and see what it does
- * if you were to change the text ("Hello World")
- */
 namespace HelloWorld
 {
+    /**
+     * Author: Kemoy Campbell       NMAD180-02
+     * Date: January 14, 2022
+     * Description: This is an hello world example.
+     *              In Programming, Hello World is often the first
+     *              program students write when learning programming
+     * Caveats: None
+     */
     class Program
     {
         static void Main(string[] args)
