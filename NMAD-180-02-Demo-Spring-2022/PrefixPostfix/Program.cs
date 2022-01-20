1710c8f8e79d728fda957aaeeb8fc1eeef18d2f7
kemoycampbell <ksc2650@rit.edu>
2022-01-20 16:06:36 -0500

    add pre and post fix examples

---
+++ NMAD-180-02-Demo-Spring-2022/PrefixPostfix/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PrefixPostfix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            int j = 5;
+
+            //  Prefix
+            int p = ++j; // j is increment before assigned to p
+
+            Console.WriteLine("prefix, should be 6: {0}",j);  // This is 6
+
+            //  Postfix
+            int y = x++; //x is assigned to y then x increment its value by 1
+
+            Console.WriteLine($"postfix, should be 5: {y}");   // This is 5
+            Console.WriteLine("X should show 6 because it was increment after assigning to y: " + x);   // This is 6
+
+        }
+    }
+}
