e65ecbacf92c54288f6acdb141fa1b86f1761af9
kemoycampbell <ksc2650@rit.edu>
2022-01-20 19:16:06 -0500

    add const and console modification demo

--- NMAD-180-02-Demo-Spring-2022/CoolConsole/Program.cs
+++ NMAD-180-02-Demo-Spring-2022/CoolConsole/Program.cs
@@ -6,13 +6,18 @@ namespace CoolConsole
     {
         static void Main(string[] args)
         {
+            Console.BackgroundColor = ConsoleColor.White;
             Console.Title = "My Million dollaz console!!!";
             Console.ForegroundColor = ConsoleColor.Green;
-            //Console.BackgroundColor = ConsoleColor.Green;
+            
             Console.WriteLine("Hello World!");
 
-            Console.ForegroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Cyan;
             Console.WriteLine("Hello whoever you are!");
+
+            //add this line so you can see the console title
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey(true);
         }
     }
 }
