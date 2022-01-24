312ee312aae1a827522f9157597b66fc664ce838
kemoycampbell <ksc2650@rit.edu>
2022-01-24 17:16:12 -0500

    currency demo

---
+++ NMAD-180-02-Demo-Spring-2022/Currency/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Currency
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int donut = 10;
+            int chocolateBar = 100;
+
+
+            Console.WriteLine("total items: {0} \nSubtotal: {1:C}", 4, (donut + chocolateBar));
+        }
+    }
+}
