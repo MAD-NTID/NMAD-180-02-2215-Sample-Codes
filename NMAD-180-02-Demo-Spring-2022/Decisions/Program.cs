0e94b15d2a5b881bc3ebd6fe6b09a2ea48505bf1
kemoycampbell <ksc2650@rit.edu>
2022-01-27 18:52:56 -0500

    decision examples

---
+++ NMAD-180-02-Demo-Spring-2022/Decisions/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace Decisions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            const int MINIMUM_HEIGHT = 10;
+            const int MINIMUM_AGE = 15;
+
+            Console.WriteLine("Insane crazy ride!!");
+            Console.Write("How old are you: ");
+            int age = int.Parse(Console.ReadLine());
+            Console.Write("How tall are you: ");
+            int height = int.Parse(Console.ReadLine());
+
+
+
+
+
+            if(age >= MINIMUM_AGE || height >= MINIMUM_HEIGHT)
+            {
+                Console.WriteLine("Enjoy the ride.... May you survive!");
+            } else
+            {
+                Console.WriteLine("Sorry buddy... come back next year!");
+            }
+
+
+            //const string USERNAME = "super";
+            //const string PASSWORD = "passy";
+            //const int DUO = 12354;
+
+            //Console.Write("Enter your username:");
+            //string username = Console.ReadLine();
+
+            //Console.Write("Enter your password:");
+            //string password = Console.ReadLine();
+
+            //Console.Write("Enter duo code:");
+            //int duo = int.Parse(Console.ReadLine());
+
+            //if(username== USERNAME && password==PASSWORD && duo == DUO)
+            //    Console.WriteLine("Welcome master!");
+            //else
+            //    Console.WriteLine("Invalid creds!");
+
+            //if(username == USERNAME)
+            //    if(password==PASSWORD)
+            //        if(duo==DUO)
+            //            Console.WriteLine("Welcome master!");
+            //        else
+            //            Console.WriteLine("Invalid duo code");
+            //    else
+            //        Console.WriteLine("You are not allowed here.. nice try!");
+            //else
+            //    Console.WriteLine("Incorrect username or password");
+            
+            //const int TOP_SECRET_PIN = 5555;
+            //const int TOP_SECRET_PIN2 = 5551;
+
+            //Console.Title = "AWESOME ATM";
+
+            //Console.Write("Enter your pin:");
+            //int inputPin = int.Parse(Console.ReadLine());
+
+            //if(inputPin == TOP_SECRET_PIN)
+            //    Console.WriteLine("Here is your money, $10000000000");
+            //else if(inputPin==TOP_SECRET_PIN2)
+            //    Console.WriteLine("Here is your money $100");
+
+            //else
+            //    Console.WriteLine("Wrong pin... No money for ya!!");
+
+
+
+            ////use to pause the program so we can see the title
+            //Console.WriteLine("Press any key to exit");
+            //Console.ReadKey(true);
+        }
+    }
+}
---
+++ NMAD-180-02-Demo-Spring-2022/JustinGetRichPlan/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace JustinGetRichPlan
+{
+    /**
+     * Author: Kemoy Campbell NMAD 180 - 02
+     * Description: Justin's plan to get rich fast
+     * Caveat: Only get $$$$ if sell 10k or more and sadly it is only 7% of total sales
+     * 
+     */
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double COMMISSION_RATE = 0.07;
+            const double MINIMUM_COMMISSION = 10000;
+
+
+            //Use Environment.Exit to quit the program 
+
+            //Console.WriteLine("Invalid input.. Exiting the program...");
+            //Environment.Exit(0);
+
+            //example of using char Y / N
+
+            //Console.WriteLine("Did you sell more than $1000 this month? (Y/N): ");
+            //char response = char.Parse(Console.ReadLine());
+
+            //if(response == 'Y' || response == 'y')
+            //{
+            //    Console.Write("How much did you help sell that car for? ");
+            //    double userInput = double.Parse(Console.ReadLine());
+            //    double profit = userInput * COMMISSION_RATE;
+            //    Console.WriteLine($"Justin made {profit:C} commission.");
+            //} else
+            //{
+            //    Console.WriteLine("Better luck next time Justin!");
+            //}
+
+
+            Console.Write("How much did you help sell that car for? ");
+            double userInput = double.Parse(Console.ReadLine());
+
+            if (userInput < MINIMUM_COMMISSION)
+            {
+                Console.WriteLine("Better luck next time Justin!");
+            }
+            else
+            {
+                double profit = userInput * COMMISSION_RATE;
+                Console.WriteLine($"Justin made {profit:C} commission.");
+            }
+        }
+    }
+}
