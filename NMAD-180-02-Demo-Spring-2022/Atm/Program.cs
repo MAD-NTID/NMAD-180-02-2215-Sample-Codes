e96a3f655e7abbfbc92590ebb674f4cb0864d05e
kemoycampbell <ksc2650@rit.edu>
2022-02-02 09:28:18 -0500

    more demo examples

---
+++ NMAD-180-02-Demo-Spring-2022/Atm/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Atm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            Console.Title = "Cool ATM";
+            Console.WriteLine("1. Fash Cash");
+            Console.WriteLine("2. Withdrawal");
+            Console.WriteLine("3. Balance Inquiry");
+            Console.WriteLine("4. Deposit");
+            Console.WriteLine("5. Transfer");
+            Console.WriteLine("6. Exit");
+
+            Console.Write("Option:");
+            string option = Console.ReadLine();
+    
+
+            switch (option)
+            {
+                case "1":
+                    Console.WriteLine("Fast cash code goes here");
+                    break;
+
+                case "2":
+                    Console.WriteLine("Withdraw some cash");
+                    break;
+                case "3":
+                    Console.WriteLine("show current balance amount");
+                    break;
+                case "4":
+                    Console.WriteLine("some money in the bank");
+                    break;
+                case "5":
+                    Console.WriteLine("The total goes down");
+                    break;
+                case "6":
+                    Console.WriteLine("Thank you for using cool atm");
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Invalid selection!!!");
+                    break;
+            }
+
+            //check to ensure the user enter a number
+            //if(int.TryParse(Console.ReadLine(), out int option))
+            //{
+
+            //} else
+            //{
+            //    Console.WriteLine("Please enter number!!!");
+            //    Environment.Exit(0);
+            //}
+
+
+
+
+
+
+            //use to show console title
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ NMAD-180-02-Demo-Spring-2022/scoped/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace scoped
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //scoped example
+            //if (true)
+            //{
+            //    int number = 1;
+            //} else
+            //{
+            //    int number = 1;
+            //}
+
+            bool isRaining = false;
+            isRaining = !isRaining;
+            isRaining = !isRaining;
+
+            Console.WriteLine(!isRaining);
+
+            const int THE_MAGIC = 1256;
+
+            Console.Write("Enter the magic number:");
+            //the number is not equal to 1256
+
+            //bool valid = ;
+            int userInput;
+            if (int.TryParse(Console.ReadLine(), out userInput))
+            {
+                if (userInput != THE_MAGIC)
+                {
+                    Console.WriteLine("Not the magic number");
+                }
+                else
+                {
+                    //the number is equal to 1256
+                    Console.WriteLine("You got it!");
+                }
+            } else
+            {
+                Console.WriteLine("Please enter a number!!!");
+            }
+   
+
+        }
+    }
+}
