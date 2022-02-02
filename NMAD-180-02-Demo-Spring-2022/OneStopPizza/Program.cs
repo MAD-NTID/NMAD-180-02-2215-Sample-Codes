5e7016b7c4885db18b60f0cb2ee2660e90940d88
kemoycampbell <ksc2650@rit.edu>
2022-02-02 16:34:45 -0500

    create pizza template for in class demo

---
+++ NMAD-180-02-Demo-Spring-2022/OneStopPizza/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OneStopPizza
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  GOAL: Use TryParse, String formatting,Logical Operators
+            //  and Nested If Statements / Maybe Negation Logical Operator
+
+            //  Create a Pizza program where users can select options for pizza toppings
+            //  Each toppings cost .50 cents - up to two toppings
+            //  Small pizza is 8 dollars
+            //  Medium pizza is 10 dollars
+            //  Large pizza is 12 dollars
+            //  New York State Tax is 8%
+            //  Calculate the total cost including tax
+        }
+    }
+}
