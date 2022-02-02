0325a97cc6de17dbb18e8518c48f9b03ad85993b
kemoycampbell <ksc2650@rit.edu>
2022-02-02 10:45:51 -0500

    update the atm example with both if and switch

--- NMAD-180-02-Demo-Spring-2022/Atm/Program.cs
+++ NMAD-180-02-Demo-Spring-2022/Atm/Program.cs
@@ -7,7 +7,18 @@ namespace Atm
         static void Main(string[] args)
         {
 
-            
+            const int OPTION_ONE = 1;
+            const int OPTION_TWO = 2;
+            const int OPTION_THREE = 3;
+            const int OPTION_FOUR = 4;
+            const int OPTION_FIVE = 5;
+            const int OPTION_SIX = 6;
+            int option;
+
+
+            /*---------------------------------
+             *    USING IF EXAMPLE             |
+             * --------------------------------*/
             Console.Title = "Cool ATM";
             Console.WriteLine("1. Fash Cash");
             Console.WriteLine("2. Withdrawal");
@@ -17,54 +28,98 @@ namespace Atm
             Console.WriteLine("6. Exit");
 
             Console.Write("Option:");
-            string option = Console.ReadLine();
-    
+
+            //did the user enter a int? if not we will inform them of the error and quit
+            if (!int.TryParse(Console.ReadLine(), out option))
+            {
+                Console.WriteLine("You are only allow to enter an integer");
+                Environment.Exit(0);
+            }
+
+            //there is no need to do this with switch because the default in switch will take care of it
+            //we need to validate that the user enter the option 1-6
+            if (option < OPTION_ONE || option > OPTION_SIX)
+            {
+                Console.WriteLine("Invalid selection, please select 1-6");
+                Environment.Exit(0);
+            }
+
+            if (option == OPTION_ONE)
+            {
+                Console.WriteLine("Fast cash code goes here");
+            }
+            else if (option == OPTION_TWO)
+            {
+                Console.WriteLine("Withdraw some cash");
+            }
+            else if (option == OPTION_THREE)
+            {
+                Console.WriteLine("show current balance amount");
+            }
+            else if (option == OPTION_FOUR)
+            {
+                Console.WriteLine("some money in the bank");
+            }
+            else if (option == OPTION_FIVE)
+            {
+                Console.WriteLine("The balance goes down");
+            }
+            else
+            {
+                Console.WriteLine("Thank you for using cool atm");
+                Environment.Exit(0);
+            }
+
+
+            /*---------------------------------
+             *    USING SWITCH EXAMPLE         |
+             * --------------------------------*/
+
+            Console.Title = "Cool ATM";
+            Console.WriteLine("1. Fash Cash");
+            Console.WriteLine("2. Withdrawal");
+            Console.WriteLine("3. Balance Inquiry");
+            Console.WriteLine("4. Deposit");
+            Console.WriteLine("5. Transfer");
+            Console.WriteLine("6. Exit");
+
+            Console.Write("Option:");
+
+            //did the user enter a int? if not we will inform them of the error and quit
+            if (!int.TryParse(Console.ReadLine(), out option))
+            {
+                Console.WriteLine("You are only allow to enter an integer");
+                Environment.Exit(0);
+            }
+
 
             switch (option)
             {
-                case "1":
+                case OPTION_ONE:
                     Console.WriteLine("Fast cash code goes here");
                     break;
 
-                case "2":
+                case OPTION_TWO:
                     Console.WriteLine("Withdraw some cash");
                     break;
-                case "3":
+                case OPTION_THREE:
                     Console.WriteLine("show current balance amount");
                     break;
-                case "4":
+                case OPTION_FOUR:
                     Console.WriteLine("some money in the bank");
                     break;
-                case "5":
+                case OPTION_FIVE:
                     Console.WriteLine("The total goes down");
                     break;
-                case "6":
+                case OPTION_SIX:
                     Console.WriteLine("Thank you for using cool atm");
                     Environment.Exit(0);
                     break;
                 default:
-                    Console.WriteLine("Invalid selection!!!");
+                    Console.WriteLine("Invalid selection, please select 1-6");
                     break;
             }
 
-            //check to ensure the user enter a number
-            //if(int.TryParse(Console.ReadLine(), out int option))
-            //{
-
-            //} else
-            //{
-            //    Console.WriteLine("Please enter number!!!");
-            //    Environment.Exit(0);
-            //}
-
-
-
-
-
-
-            //use to show console title
-            Console.WriteLine("Press any key to exit");
-            Console.ReadKey(true);
         }
     }
 }
