a6936cb41402edb75cc00ebe62a5a36c26941e86
kemoycampbell <ksc2650@rit.edu>
2022-02-03 18:22:10 -0500

    switch and more if

---
+++ NMAD-180-02-Demo-Spring-2022/GradeLetter/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace GradeLetter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int GRADE_LETTER_A = 90;
+            const int HUNDRED = 100;
+            const int GRADE_LETTER_B = 80;
+            const int GRADE_LETTER_C = 70;
+            const int GRADE_LETTER_D = 60;
+            const int ZERO = 0;
+
+            Console.Write("What is your grade:");
+
+ 
+
+            //user entered a valid number
+            if (int.TryParse(Console.ReadLine(), out int grade))
+            {
+                if(grade>=0 && grade<=HUNDRED)
+                {
+                    if (grade >= GRADE_LETTER_A && grade <= HUNDRED)
+                    {
+                        Console.WriteLine("You have got an A grade");
+                    }
+                    else if (grade >= GRADE_LETTER_B)
+                    {
+                        Console.WriteLine("You have got an B grade");
+                    }
+                    else if (grade >= GRADE_LETTER_C)
+                    {
+                        Console.WriteLine("You have got an C grade");
+                    }
+                    else if (grade >= GRADE_LETTER_D)
+                    {
+                        Console.WriteLine("You have got an D grade");
+                    } else
+                    {
+                        Console.WriteLine("You have got an F grade");
+                    }
+                } else
+                {
+                    Console.WriteLine($"Please enter a grade between {ZERO}-{HUNDRED}");
+                }
+ 
+            } else
+            {
+                Console.WriteLine("Invalid input!");
+            }
+        }
+    }
+}
---
+++ NMAD-180-02-Demo-Spring-2022/inception/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace inception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            int option = int.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+   
+                case 1:
+                case 2:
+                    Console.WriteLine($"I am  option {option} and I jumped in the dream #2");
+                    break;
+                default:
+                    Console.WriteLine("I have no idea who you are.... turn around");
+                    break;
+            }
+        }
+    }
+}
