bd3b244a53f1b6d6b0e7df982eeb6e613c91d075
kemoycampbell <ksc2650@rit.edu>
2022-02-17 16:54:08 -0500

    add array and loop demo

---
+++ NMAD-180-02-Demo-Spring-2022/Array/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //we create a variable to hold an array of 5 numbers
+            int[] numbers = new int[5];
+
+            //array are index are 0 based so we start counting from 0
+            //assigning numbers to the array indexes
+            Console.WriteLine("Example of hardcoded array values");
+            numbers[0] = 2;
+            numbers[1] = 5;
+            numbers[2] = 7;
+            numbers[3] = 9;
+            numbers[4] = 10;
+
+            Console.WriteLine($"Item 0 : {numbers[0]}");
+            Console.WriteLine($"Item 1 : {numbers[1]}");
+            Console.WriteLine($"Item 2 : {numbers[2]}");
+            Console.WriteLine($"Item 3 : {numbers[3]}");
+            Console.WriteLine($"Item 4 : {numbers[4]}");
+
+
+            //example of taking a input from the user and assign it to the first index in the array
+            Console.WriteLine("\nExample of taking input from the user and add to array");
+            int[] nums = new int[3];
+            Console.Write("Enter a number to add to the first index in the array:");
+            int input = int.Parse(Console.ReadLine());
+            nums[0] = input; // take the first number and assign it to the first element in the array
+
+            //assign 3 and 6 to the other two indexes
+            nums[1] = 3;
+            nums[2] = 6;
+
+            Console.WriteLine($"The sum of all 3 numbers {input} + 3 + 6 = {nums[0] + nums[1] + nums[2]}");
+
+
+        }
+    }
+}
---
+++ NMAD-180-02-Demo-Spring-2022/Loop/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /**
+             * Remember programming typically start counting at 0
+             */
+            Console.WriteLine("Example of using while loop");
+            int counter = 0;
+            while (counter < 10)
+            {
+                Console.WriteLine(counter);
+                counter++;
+            }
+
+            //for loop example
+            Console.WriteLine("\nExample of using for loop");
+            for (int counter2 = 0; counter2 < 10; counter2++)
+            {
+                Console.WriteLine(counter2);
+            }
+
+            //example of using a while to take input
+            Console.WriteLine("Example of using a while loop to take inputs from the user");
+            double[] grades = new double[5];
+            int counter3 = 0;
+            while (counter3 < 5)
+            {
+                Console.Write($"Enter grade {counter3 + 1}:");
+                //double grade = double.Parse(Console.ReadLine());
+
+                grades[counter3] = double.Parse(Console.ReadLine());
+                counter3++;
+            }
+
+
+            //for loop to show all grades
+            Console.WriteLine("Example of using a for loop to show all the entered grades");
+            double sum = 0;
+            for (int index = 0; index < grades.Length; index++)
+            {
+                sum += grades[index];
+
+                Console.WriteLine($"grade {index + 1} : {grades[index]}");
+            }
+
+            Console.WriteLine("Sum: {0}", sum);
+            Console.WriteLine("Average: {0}", sum / grades.Length);
+        }
+    }
+}
