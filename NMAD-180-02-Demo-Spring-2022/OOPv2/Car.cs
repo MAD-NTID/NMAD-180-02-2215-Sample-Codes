a99033ce97b91c34e9670db5e0bfce63ebd04841
kemoycampbell <ksc2650@rit.edu>
2022-03-03 18:29:36 -0500

    more oop

---
+++ NMAD-180-02-Demo-Spring-2022/OOPv2/Car.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPv2
+{
+    class Car
+    {
+        //private string engine;
+         private string brand="N/A";
+        //private string color;
+         private int doorNumbers = -1;
+        //private int year;
+
+        public int DoorNumbers { 
+            get {
+                return doorNumbers; 
+            } 
+            set {
+                if (value > 0 && value <= 4)
+                    doorNumbers = value;
+            } 
+        }
+        public string Brand
+        {
+            get
+            {
+                return brand;
+            }
+            set
+            {
+                if (value != "")
+                    brand = value;
+            }
+        }
+        public int Year { get; set; }
+
+    }
+}
---
+++ NMAD-180-02-Demo-Spring-2022/OOPv2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace OOPv2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car[] cars = new Car[2];
+
+            for(int i = 0; i< cars.Length; i++)
+            {
+                Console.Write("Brand:");
+                string brand = Console.ReadLine();
+                Console.Write("Year:");
+                int year = int.Parse(Console.ReadLine());
+
+                Console.Write("Doors:");
+                int doors = int.Parse(Console.ReadLine());
+
+                Car car = new Car();
+                car.Brand = brand;
+                car.Year = year;
+                car.DoorNumbers = doors;
+
+                cars[i] = car;
+            }
+
+            Console.WriteLine("\nCar info:");
+
+            foreach(Car car in cars)
+            {
+                Console.WriteLine($"{car.Brand} - {car.Year} - {car.DoorNumbers}");
+            }
+
+
+        }
+    }
+}
