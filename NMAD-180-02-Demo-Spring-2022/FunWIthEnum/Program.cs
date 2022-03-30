83dcd1d7341f4db25c04cc9b4db93ed5a5c7c25d
kemoycampbell <ksc2650@rit.edu>
2022-03-30 18:25:41 -0400

    enum examples

---
+++ NMAD-180-02-Demo-Spring-2022/FunWIthEnum/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace FunWIthEnum
+{
+    class Program
+    {
+        enum Options
+        {
+            OptionOne = 1,
+            OptionTwo,
+            OptionThree,
+            OptionFour,
+            OptionFive
+        }
+
+        static void Main(string[] args)
+        {
+
+            string[] labels = Enum.GetNames(typeof(Options));
+            //t[] values = Enum.GetValues(typeof(Options));
+            foreach(string label in labels)
+            {
+                Console.WriteLine(label);
+            }
+            while(true)
+            {
+                Console.Write("Enter a option:");
+                string inputString = Console.ReadLine();
+                int inputInt = int.Parse(inputString);
+
+                Options input = (Options)inputInt;
+
+                Console.WriteLine((int) input);
+            }
+
+   
+            //Console.WriteLine($"{Options.OptionFour} is {(int)Options.OptionFour}");
+        }
+    }
+}
